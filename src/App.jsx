dd6ab19e6639a2927e5a66d7f84c9d050a1c0083
IgorDyshuk <igordysuk@gmail.com>
2026-04-22 17:39:07 +0300

    feat(about): add About section with line-by-line reveal and exit animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import HomeSection from "./Sections/HomeSection";
+import AboutSection from "./Sections/AboutSection";
 
 function App() {
   return (
-    <div className="px-5 sm:px-0 mx-auto w-full max-w-3xl xl:max-w-4xl">
+    <div className="px-5 sm:px-0 mx-auto w-full max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl">
       <HomeSection />
+      <AboutSection />
+      <div className="h-200 w-full border-red-500 border-2" />
     </div>
   );
 }
